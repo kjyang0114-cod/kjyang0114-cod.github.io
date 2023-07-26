e8535f4e8674ad9add22903dced42af93e9c1ae1
kjyang0114-cod <123161546+kjyang0114-cod@users.noreply.github.com>
2023-07-27 06:04:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// // console.log("Hello,JS")
+// let name = "yee"
+// console.log(name)
+
+// let sentence = "nmsl"
+// console.log(name + sentence)
+
+// fruit = prompt("你最喜歡的水果是甚麼?")
+// alert(fruit)
+
+// let food = parseInt(prompt("你今天吃多少錢"))
+// let tip = parseInt(prompt("小費有幾%?")) / 100
+// let tipamount = food * tip
+// let tiptotal = food + tipamount
+// console.log("小費金額:"+tipamount)
+// console.log("總金額:"+ tiptotal)
+
+// let x = parseInt(prompt("x是多少"))
+// let y = parseInt(prompt("y是多少"))
+// let xy = x + y 
+// let xy2 = xy / 2
+// console.log("x跟y的中間項為:"+xy2)
+
+//<br><br>
+//    <img src="https://s.yimg.com/ny/api/res/1.2/wf3.2DKrrGC35X8UEwQODA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTU0MTtjZj13ZWJw/https://media.zenfs.com/en/mirrormedia.mg/a1e390f4524e5d4f936bad6eaf12acc3" style="display: block;border-style:solid;" /> 
+//    <p id="demo"></p>
